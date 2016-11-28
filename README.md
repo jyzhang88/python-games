24bd26016be2a8ed1608e4afa6ef587b68f32b68
jyzhang88 <zhangjy.jinyan@gmail.com>
2016-11-28 11:22:07 +0800

    updated tictactoe.py

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Repository for games written using Python
 
 A personal repository where I upload Python codes for some games. It's just for personal challenge, and more importantly to improve on my Python coding skills. Hopefully one day I'll look back and edit the codes to make them more efficient. 
+
+
+### tictactoe.py
+
+Import `tictactoe.py` and run `lets_play()` function to initiate the game.
+The flowthrough of the game:
+1. Determine who starts first, **X** or **O**
+2. Print the board
+3. Player enters a digit to mark the choice
+4. Check if digit already taken
+5. If not, replace digit with X or O
+6. Print the board
+7. Check if someone has won the game
+8. If not, repeat steps 3-7 until someone wins the game
+9. The loop ends when someone wins the game
+10. It is a tie if no one wins the game
--- tictactoe.py
+++ tictactoe.py
@@ -1,35 +1,37 @@
-# work in progress
+# Game of Tic-Tac-Toe
 
 def lets_play():
-	print("Welcome to the Tic-Tac-Toe!")
+	print("Welcome to the Tic-Tac-Toe! \n")
 
 	# Define the variables needed
 	keypad = {0: ["1", "2", "3"],
 	          1: ["4", "5", "6"],
 	          2: ["7", "8", "9"]}
+
+	# Define the winning combinations to check against
 	q = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 	winnings = {0: q[0:3], 1: q[3:6], 2: q[6:9],
 	            3: q[0::3], 4: q[1::3], 5: q[2::3],
 	            6: q[0::4], 7: q[2:7:2]}
 
-	# Define all functions first
 
-	def choose_first():
+	# Define the functions needed
+	def choose_first(): # choose either X or O to go first
 		import random
-		global turn
 		turn = random.sample(["X", "O"], 1)[0]
 		print("Player with {} goes first.".format(turn))
+		return turn
 
-	def print_board():
+	def print_board():  # print the board out
 		for i in keypad:
 			print("|".join(keypad[i]))
 
-	def check_win():
+	def check_win():    # check if someone has won the game
 		lst = []
 		global keypoint
 		keypoint = "string"
 		for key, value in keypad.items():
-			lst.extend(keypad[value])
+			lst.extend(value)
 		board_row = "".join(lst)
 
 		for sn, ls in winnings.items():
@@ -40,54 +42,55 @@ def lets_play():
 
 			triplet = "".join(triplet)
 
-			if triplet == output * 3:
-				print("You've won!")
+			# checking the board against the winning combination
+			if triplet == turn * 3:
+				print("{} has won!".format(turn))
 				keypoint = "Exit game"
 				break
 			elif sn == 7:
-				print("Please continute with the boring game...")
+				print("Next player's choice...")
 
-	def check_repeat():
+	def check_repeat(): # check if the number is already taken
 		lste = []
 		for key, value in keypad.items():
 			lste.extend(value)
-		while str(choice) not in lste:
-			choice = input("Please try again.")
+		while True:
+			global choice
+			choice = input("Mark the {}".format(turn))
+			if choice not in lste:
+				print("Number is already used, please try again")
+				continue
+			else:
+				break
 
-	choose_first()
-
+	turn = choose_first()
 	print_board()
 
-	for i in range(9):
-		if turn == "X" and i in range(9):
-			choice = input("Mark the X")
+	for i in range(9):  # enters the loop of the game
+		if turn == "X" and i in range(8):
 			check_repeat()
-		elif turn == "O" and i in range(g):
-			choice = input("Mark the O")
+		elif turn == "O" and i in range(8):
 			check_repeat()
 		else:
 			print("This game is a tie!")
 			break
 
-		# based on choice from above, replace number with choice
+		# replacing the numbers with X or O
 		for row in keypad:
 			if choice in keypad[row]:
 				new = [num.replace(choice, turn) for num in keypad[row]]
 				break
 		keypad[row] = new
 
+		check_win()
 		print_board()
 
-		check_win()
-
+		# if someone has won the game, exit out of the game loop
 		if keypoint == "Exit game":
 			break
 
-
-
-# Error 1: repeat number
-# Error 1 Message: "The box is already filled, please pay attention!"
-
-# Write code to play with AI
-# use random number generator -> check within leftover number
-
+		# if the game has not ended, alternate between X and O
+		if turn == "X":
+			turn = "O"
+		elif turn == "O":
+			turn = "X"
